a974c7a63fe6de1c060b0e20c0fdd3875038b68e
Mikuto Taguchi <mikuto@example.com>
2026-07-14 17:59:10 +0900

    辞書APIのコード作成途中

--- src/main/java/com/example/wordfall/controller/WordController.java
+++ src/main/java/com/example/wordfall/controller/WordController.java
@@ -13,6 +13,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 // ↓ ここに@RestControllerを付ける(JSONを返すControllerだったことを思い出してください)
 @RestController
@@ -40,6 +43,30 @@ public class WordController {
     @GetMapping("/api/check-word")
     public boolean returnWords(@RequestParam String word) {
         return words.contains(word.toUpperCase());
+    }
+
+    //外部の辞書APIを呼び出すための道具
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    //JSON文字列を解析するための道具
+    private final Object objectMapper = new ObjectMapper();
+
+    @GetMapping("/api/meaning")
+    public MeaningResponse getMeaning(@RequestParam String word) {
+        try{
+            //1.外部の辞書APIを呼び出す
+            String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word.toLowerCase();
+            String json = restTemplate.getForObject(url,String.class);
+
+            // 2.JSON文字列を解析する
+            JsonNode root = objectMapper.readTree(json);
+            JsonNode firstEntry = firstEntry.get("meanings").get(0);
+            String partOfSpeech = firstMeaning.get("partOfSpeech").asText();
+            String definition = firstMeaning.get("definitions").get(0).get("definition").asText();
+
+            //3.シンプルな形にして返す
+            return new MeaningResponse(word.toUpperCase(), partOfSpeech, definition)
+        }
     }
 
 }
